fe283d7a6596848870ee3a6d095d50760afdca91
ShyBoy <tjghdudrnt@naver.com>
2025-04-20 12:38:39 +0900

    feat: Add Imports.sol

---
+++ contracts/Imports.sol
@@ -0,0 +1,5 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
