67909ab39c9dc993122755e207aa691b78b1de85
ShyBoy <tjghdudrnt@naver.com>
2025-04-28 23:33:30 +0900

    feat: Change SellerRegistry to Event Emitting Contract:

--- contracts/SellerRegistry.sol
+++ contracts/SellerRegistry.sol
@@ -12,19 +12,32 @@ contract SellerRegistry is OwnableERC2771Context {
     mapping (address => bool) public isActiveSeller;
     mapping (address => mapping(address => bool)) public paymentChannels;
 
+    event SellerRegistered (
+        address indexed seller, 
+        address[] paymentChannels,
+        bool indexed isSuccess);
+
+    event PaymentChannelsAdded (
+        address indexed seller,
+        address[] paymentChannels);
+
+    event PaymentChannelsRemoved (
+        address indexed seller,
+        address[] paymentChannels);
+
     constructor (address owner_, address forwarder) 
         OwnableERC2771Context(owner_, forwarder)
     {}
 
     /// @notice Register a new seller(an address which called this function), 
     /// with a list of ERC-20 tokens as his/her/their/its payment channels
+    /// Emits SellerRegistered event
     /// @param paymentChannels_ List of ERC-20 tokens which will be used as 
     /// payment channels
     function registerSeller (
         address[] calldata paymentChannels_
     )
         external
-        returns (bool isSuccess)
     {
         require(
             paymentChannels_.length != 0,
@@ -37,11 +50,12 @@ contract SellerRegistry is OwnableERC2771Context {
             paymentChannels[_msgSender()][paymentChannels_[i]] = true;
         }
 
-        return true;
+        emit SellerRegistered(_msgSender(), paymentChannels_, true);
     }
 
     /// @notice
     /// Add new payment channels available for seller's address
+    /// Emits PaymentChannelsAdded event
     /// @param paymentChannels_ ERC-20 tokens' address list to be added
     function addPaymentChannels (
         address[] calldata paymentChannels_
@@ -57,11 +71,14 @@ contract SellerRegistry is OwnableERC2771Context {
             
             paymentChannels[_msgSender()][paymentChannels_[i]] = true;
         }
+
+        emit PaymentChannelsAdded(_msgSender(), paymentChannels_);
     }
 
     /// @notice 
     /// Remove some or all of payment channels 
     /// which were available for the address that called this function
+    /// Emits PaymentChannelsRemoved event
     /// @param paymentChannels_ ERC-20 tokens' address list to be disabled
     function removePaymentChannels (
         address[] calldata paymentChannels_
@@ -77,6 +94,8 @@ contract SellerRegistry is OwnableERC2771Context {
                 delete paymentChannels[_msgSender()][paymentChannels_[i]];
             }
         }
+
+        emit PaymentChannelsRemoved(_msgSender(), paymentChannels_);
     }
 
 }
