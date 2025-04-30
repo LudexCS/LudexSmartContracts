50eb3e774ec4f54f7205dce750e2947af2176c0f
ShyBoy <tjghdudrnt@naver.com>
2025-04-30 09:34:59 +0900

    fix: No cache cleaning for postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,27 +1,37 @@
 const { execSync } = require("child_process");
+const path = require("path");
+const rimraf = require("rimraf");
 
 function run(command) {
   try {
     execSync(command, { stdio: "inherit" });
   } catch (error) {
     console.error(`Error running command "${command}"`);
-    throw error;
+    process.exit(1);
   }
 }
 
-async function main() {
-  try {
-    console.log("Running Hardhat compile...");
-    run("npx hardhat compile");
+function clean(dir) {
+  rimraf.sync(dir); // OS 호환 완전 삭제
+}
 
-    console.log("Running TypeScript compile...");
-    run("npx tsc");
+function main() {
+  const root = path.resolve(__dirname, "..");
 
-    console.log("Postinstall complete.");
-  } catch (err) {
-    console.error("Postinstall script failed.");
-    process.exit(1);
-  }
+  console.log("Cleaning build artifacts...");
+  clean(path.join(root, "build"));
+  clean(path.join(root, "artifacts"));
+  clean(path.join(root, "cache"));
+  clean(path.join(root, "typechain-types"));
+  clean(path.join(root, "dist"));
+
+  console.log("Running Hardhat compile...");
+  run("npx hardhat compile");
+
+  console.log("Running TypeScript compile...");
+  run("npx tsc");
+
+  console.log("Postinstall complete.");
 }
 
 main();
