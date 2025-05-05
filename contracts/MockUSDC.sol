d57a4cb91372c2fdbb39ed1ea510a915f22e2c58
ShyBoy <tjghdudrnt@naver.com>
2025-05-06 00:42:20 +0900

    fix: Wrong inheritance of MockUSDC (inheriting ERC20, not ERC20Permit)

--- contracts/MockUSDC.sol
+++ contracts/MockUSDC.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MockUSDC is ERC20 {
+contract MockUSDC is ERC20Permit {
     uint256 public constant INITIAL_AMOUNT = 1_000_000 * 10 ** 6; // 1 million USDC
 
-    constructor(address[] memory recipients) ERC20("MockUSDC", "USDC") {
+    constructor(address[] memory recipients) 
+        ERC20("MockUSDC", "USDC")
+        ERC20Permit("MockUSDC") 
+    {
         for (uint256 i = 0; i < recipients.length; i++) {
             _mint(recipients[i], INITIAL_AMOUNT);
         }
