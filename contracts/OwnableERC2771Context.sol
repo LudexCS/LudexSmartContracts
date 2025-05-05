ebea74e8e273f6de14b55a63fe3de4e034099fc7
ShyBoy <tjghdudrnt@naver.com>
2025-05-06 06:26:12 +0900

    fix: No virtual modifiers for functions of OwnableERC2771Context.sol

--- contracts/OwnableERC2771Context.sol
+++ contracts/OwnableERC2771Context.sol
@@ -1,5 +1,5 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -7,36 +7,38 @@ import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 abstract contract OwnableERC2771Context is Ownable, ERC2771Context {
 
-    constructor (address owner_, address forwarder) 
+    constructor(address owner_, address forwarder) 
         Ownable(owner_)
         ERC2771Context(forwarder)
     {}
-     
-    function _msgSender () 
+
+    function _msgSender() 
         internal 
         view 
-        override (Context, ERC2771Context)
+        virtual 
+        override(Context, ERC2771Context) 
         returns (address) 
     {
         return ERC2771Context._msgSender();
     }
 
-    function _msgData ()
-        internal
-        view
-        override (Context, ERC2771Context)
-        returns (bytes calldata)
+    function _msgData() 
+        internal 
+        view 
+        virtual 
+        override(Context, ERC2771Context) 
+        returns (bytes calldata) 
     {
         return ERC2771Context._msgData();
     }
 
-    function _contextSuffixLength()
-        internal
-        view
-        override (Context, ERC2771Context)
-        returns (uint256)
+    function _contextSuffixLength() 
+        internal 
+        view 
+        virtual 
+        override(Context, ERC2771Context) 
+        returns (uint256) 
     {
         return ERC2771Context._contextSuffixLength();
     }
-
 }
