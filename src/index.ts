a4d1f922d8f660220f1936f029dd94a589ad858e
shyboy <tjghdudrnt@naver.com>
2025-05-07 13:34:40 +0900

    feat: Add export of PaymentProcessor ABI(index.ts)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const ERC20PermitJson = require("../../build/contracts/@openzeppelin/contracts/t
 const ERC2771ForwarderJson = require("../../build/contracts/@openzeppelin/contracts/metatx/ERC2771Forwarder.sol/ERC2771Forwarder.json");
 const StoreJson = require("../../build/contracts/contracts/Store.sol/Store.json");
 const PriceTableJson = require("../../build/contracts/contracts/PriceTable.sol/PriceTable.json");
+const PaymentProcessorJson = require("../../build/contracts/contracts/PaymentProcessor.sol/PaymentProcessor.json");
 const LedgerJson = require("../../build/contracts/contracts/Ledger.sol/Ledger.json");
 const SellerRegistryJson = require("../../build/contracts/contracts/SellerRegistry.sol/SellerRegistry.json");
 const ItemRegistryJson = require("../../build/contracts/contracts/ItemRegistry.sol/ItemRegistry.json");
@@ -14,6 +15,7 @@ export namespace LudexContract {
         export const ERC2771Forwarder = ERC2771ForwarderJson.abi;
         export const Store = StoreJson.abi;
         export const PriceTable = PriceTableJson.abi;
+        export const PaymentProcessor = PaymentProcessorJson.abi;
         export const Ledger = LedgerJson.abi;
         export const SellerRegistry = SellerRegistryJson.abi;
         export const ItemRegistry = ItemRegistryJson.abi;
